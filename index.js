1b0723ecbedd396cf6698b4442868efa08c99791
mclarke661 <mclarke63@qub.ac.uk>
2023-04-20 00:07:59 +0100

    artwork added

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const express = require("express");
 const app = express();
 const connection = require("./connection.js");
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running at port 3000");
-});
-
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
@@ -32,7 +28,24 @@ db.connect((err) => {
 
 module.exports = db;
 
-app.get('/stacks_of_wax', (req, res) => {
+app.get("/select",(req,res) => {
+  let readsql = "SELECT * FROM tv_shows";
+  connection.query(readsql,(err, rows)=>{
+      if(err) throw err;
+      let stringdata = JSON.stringify(rows);
+      res.send(`<h2>My TV</h2><code> ${stringdata}</code>`);
+  });
+});
+
+app.get("/row",(req,res) => {
+  let readsql = "SELECT * FROM tv_shows";
+  connection.query(readsql,(err, rows)=>{
+      if(err) throw err;
+      res.send(`<h2>My TV</h2><code>${rows[1].showname}</code><img src='${rows[1].imgpath}'>`);
+  });
+});
+
+app.get('/album_art', (req, res) => {
   const sql = 'SELECT album_art FROM album';
   db.query(sql, (err, result) => {
     if (err) throw err;
@@ -41,7 +54,7 @@ app.get('/stacks_of_wax', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+app.listen(process.env.PORT || 3000, () => { 
+  console.log("Server is running at port 3000"); 
 });
 
