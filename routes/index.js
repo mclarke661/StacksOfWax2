6f415d634782290ee39d6249301238d76feae627
mclarke661 <mclarke63@qub.ac.uk>
2023-04-24 17:42:19 +0100

    .gitignore edit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
+const app = require('express')();
+const basicAuth = require('express-basic-auth');
 
 // app.get("/", (req, res) => { 
 //     res.send(`<h1>conference</h1>
@@ -7,4 +9,30 @@ const router = express.Router();
 //             <p><a href='/speakers/John'>John Busch</a></p>`);
 //   });
 
-  modules.exports = router;
+const auth = basicAuth({
+    users: { 'admin': 'admin123' },
+    unauthorizedResponse: 'not authorized'});
+
+
+app.get('/protected', auth , (req, res) => {
+res.json({
+          data:"Welcome to my API",
+          message:"Not protected data",
+          auth:"header authorization"
+          });
+});
+
+
+app.get('/', (req, res) => {
+ res.json({
+          data:"Welcome to my API",
+          message:"Not protected data"
+          });
+});
+
+
+app.listen(4000, () =>{
+console.log('host 4000');
+});
+
+// modules.exports = router;
