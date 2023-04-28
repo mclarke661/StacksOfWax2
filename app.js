3584348c3c37832e9e9d9f0fa04a39dd175cc111
mclarke661 <mclarke63@qub.ac.uk>
2023-04-28 16:26:34 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// require('dotenv').config();
 const express = require("express");
 const app = express();
 const mysql = require('mysql');
@@ -7,10 +6,7 @@ const db = require("./connection.js");
 const axios = require('axios');
 
 
-// const routes = require('./routes/')
-
 app.use(express.static('static'));
-const PORT = process.env.PORT || 4000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
 // Parse URL-encoded bodies (as sent by HTML forms)
@@ -20,123 +16,14 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.get('/sign', (request, response) => {
-  console.log(request.body.emailField);
-  response.render('forms');
-  });
-  
-  app.post('/sign', (request, response) => {
-    response.render('forms');
-  });
-
-app.get("/row", (req, res) => {
-  let showid = req.query.album_id;
-  let readsql = "SELECT * FROM album WHERE id = ? ";
-
-  db.query(readsql, [album_id], (err, rows) => {
-    if (err) throw err;
-    let album = {
-      name: rows[0]['album_name'],
-      imgp: rows[0]['album_art'],
-      year: rows[0]['album_year'],
-    }
-    res.render('album', { album });
-  });
-});
-
-app.post("/create", (req, res) => {
-
-  let senttitle = req.body.fieldTitle;
-  let sentimg = req.body.fieldImg;
-  let sentdes = req.body.fieldDescr;
-
-  const showData = {
-    title: senttitle,
-    img: sentimg,
-    description: sentdes,
-  };
-
-  const config = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }
-
-  let epoint = "http://jbusch.webhosting2.eeecs.qub.ac.uk/tvapi/?create&apikey=***";
-
-  axios.post(epoint, showData, config).then((response) => {
-    console.log(response.data);
-    res.render('add', { showData });
-  }).catch((err) => {
-    console.log(err.message);
-  });
-});
-
-app.get("/select", (req, res) => {
-  let readsql = "SELECT * FROM album";
-  db.query(readsql, (err, rows) => {
-    if (err) throw err;
-    let rowdata = rows;
-    res.render('albums', { title: 'List of albums', rowdata });
-  });
-});
-
-// Express route handler with URL: '/products' and a handler function 
-app.get('/products', (request, response) => {
-  // Make the GET call by passing a config object to the instance  
-  axios.get('https://fakestoreapi.com/products?limit=5').then(apiResponse => {
-    // process the response  
-    const products = apiResponse.data;
-    response.json(products);
-  });
-});
-
-/*app.get("/select",(req,res) => {
-  let readsql = "SELECT * FROM tv_shows";
-  connection.query(readsql,(err, rows)=>{
-      if(err) throw err;
-      let stringdata = JSON.stringify(rows);
-      res.send(`<h2>My TV</h2><code> ${stringdata}</code>`);
-  });
-});
-
-app.get("/row",(req,res) => {
-  let readsql = "SELECT * FROM tv_shows";
-  connection.query(readsql,(err, rows)=>{
-      if(err) throw err;
-      res.send(`<h2>My TV</h2><code>${rows[1].showname}</code><img src='${rows[1].imgpath}'>`);
-  });
-});
-*/
-
-  // album art
-  // app.get('/', (req, res) => {
-  //   db.query('SELECT album_art FROM album ORDER BY RAND() LIMIT 1', (err, result) => {
-  //     if (err) throw err;
-  //     const imageUrl = result[0].url;
-  //     res.render('index', { imageUrl });
-  //   });
-  // });
-
-app.get('/', (req, res) => {
+app.get('/album_art', (req, res) => {
   const sql = 'SELECT album_art FROM album';
   db.query(sql, (err, result) => {
     if (err) throw err;
     const albumArtUrls = result.map((row) => row.album_art);
-    // res.json(albumArtUrls);
-    res.render('index', { albumArtUrls: albumArtUrls });
+    res.json(albumArtUrls);
   });
 });
-
-function handleKeyPress(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    event.target.form.submit();
-  }
-}
-// app.use('/', routes);
-// app.use('/account', userRoute);
-// app.use('/review', reviewRoute)
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running at port 3000");
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 // const express = require('express');
 // const router = express.Router();
-const app = require('express')();
-const basicAuth = require('express-basic-auth');
+const express = require('express');
+let app = express();
+// const basicAuth = require('express-basic-auth');
 
 // app.get("/", (req, res) => { 
 //     res.send(`<h1>conference</h1>
@@ -9,31 +10,64 @@ const basicAuth = require('express-basic-auth');
 //             <p><a href='/speakers/John'>John Busch</a></p>`);
 //   });
 
-//basic security
-const auth = basicAuth({
-    users: { 'admin': 'admin123' },
-    unauthorizedResponse: 'not authorized'});
+// //basic security
+// const auth = basicAuth({
+//     users: { 'admin': 'admin123' },
+//     unauthorizedResponse: 'not authorized'});
 
 
-app.get('/protected', auth , (req, res) => {
-res.json({
-          data:"Welcome to my API",
-          message:"Not protected data",
-          auth:"header authorization"
-          });
-});
+// app.get('/protected', auth , (req, res) => {
+// res.json({
+//           data:"Welcome to my API",
+//           message:"Not protected data",
+//           auth:"header authorization"
+//           });
+// });
 
 
-app.get('/', (req, res) => {
- res.json({
-          data:"Welcome to my API",
-          message:"Not protected data"
-          });
-});
+// app.get('/', (req, res) => {
+//  res.json({
+//           data:"Welcome to my API",
+//           message:"Not protected data"
+//           });
+// });
 
 
-app.listen(4000, () =>{
-console.log('host 4000');
-});
+// app.listen(4000, () =>{
+// console.log('host 4000');
+// });
 
 // modules.exports = router;
+
+
+
+//middleware to use the EJS template engine
+app.set('view engine', 'ejs');
+
+//middleware to be able POST <form> data 
+app.use(express.urlencoded({extended: true}));
+
+//store favourite band in a global variable
+let favband = "not choosen yet";
+
+app.get('/fav',  (req, res) => {
+    res.render('band', {data: favband});
+});
+
+app.post('/fav',  (req, res) => {
+    //change global variable favband to the value of the text field
+    favband = req.body.favourite;
+    res.render('band', {data: favband});
+});
+
+app.get('/page1',  (req, res) => {
+    res.render('page1', {data: favband});
+});
+
+app.get('/page2',  (req, res) => {
+    res.render('page2', {data: favband});
+});
+
+app.listen(process.env.PORT || 4000, ()=>{ 
+    console.log("server started on: localhost:4000/fav");
+});
