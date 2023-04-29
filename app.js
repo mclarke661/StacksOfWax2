6563bffe63f3b0e515dee8cac004441dd07b46c0
mclarke661 <mclarke63@qub.ac.uk>
2023-04-29 21:25:48 +0100

    updates

--- app.js
+++ app.js
@@ -1,22 +1,112 @@
-const path = require('path');
+
 const express = require("express");
 const app = express();
 const mysql = require('mysql');
-const db = require("./connection.js");
 const axios = require('axios');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+const db = require('./connection.js');
 
-app.use(express.static('static'));
+//middleware to config session data
+app.use(sessions({
+  secret: 'thisisasecret',
+  saveUninitialized: false,
+  resave: false
+})
+);
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, './public')));
-// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/albums', (req, res) => {
+
+  const sql = 'SELECT * FROM album';
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    let rowdata = rows;
+    res.render('albums', { rowdata })
+
+  });
+});
+
+app.get("/album", (req, res) => {
+
+  let album_id = req.query.album_id;
+
+  let sql = "SELECT * FROM album WHERE album_id = ? ";
+
+  db.query(sql, [album_id], (err, rows) => {
+    if (err) throw err;
+    let album = {
+      name: rows[0]['album_name'],
+      img: rows[0]['album_art'],
+      year: rows[0]['album_year']
+    };
+    res.render('album', { album });
+  });
 });
 
 app.get("/login", (req, res) => {
   res.render("login.ejs");
+});
+
+app.post('/login', (req, res) => {
+  let useremail = req.body.emailField;
+
+  let checkuser = 'SELECT * FROM my_users WHERE email = ? ';
+
+  db.query(checkuser, [useremail], (err, rows) => {
+    if (err) throw err;
+    let numRows = rows.length;
+    if (numRows > 0) {
+      res.redirect('/profile');
+    } else {
+      res.redirect('/');
+    }
+  });
+
+});
+
+app.get("/admin/addAlbum", (req, res) => {
+  res.render("addalbum.ejs");
+});
+
+app.post("/admin/addAlbum", (req, res) => {
+
+  let sentalbum = req.body.fieldAlbum;
+  let sentyear = req.body.fieldYear;
+  let sentartist = req.body.fieldArtist;
+  let sentgenre = req.body.fieldGenre;
+  let sentsubgenre = req.body.fieldSubgenre;
+  let sentsubgenre2 = req.body.fieldSubgenre2;
+  let sentrecordcompany = req.body.fieldRecordCompany;
+  let sentalbumart = req.body.fieldAlbumArt;
+
+  const showData = {
+    album: sentalbum,
+    year: sentyear,
+    artist: sentartist,
+    genre: sentgenre,
+    subgenre: sentsubgenre,
+    subgenre2: sentsubgenre2,
+    recordCompany: sentrecordcompany,
+    albumArt: sentalbumart,
+  };
+
+  axios.post(endp, showData, config).then((response) => {
+    console.log(response.data);
+    res.render('addalbum', { showData });
+  }).catch((err) => {
+    console.log(err.message);
+  });
+});
+
+app.get("/admin/editalbum", (req, res) => {
+  res.render("editalbum.ejs");
 });
 
 app.get("/signup", (req, res) => {
@@ -36,7 +126,27 @@ app.get('/album_art', (req, res) => {
   });
 });
 
+app.get('/myalbums', (req, res) => {
+  //get at the session object and store it ina local variable
+  let sess_obj = req.session;
+  //check to see if the key favband exists. If not then set if with the
+  //string 'not choosen yet'
+  if (!sess_obj.favalbum) {
+    sess_obj.favalbum = "not chosen yet";
+  }
+  //send the session object key favalbum to the band.ejs template
+  res.render('myalbums', { data: sess_obj.favalbum });
+});
+
+app.post('/myalbums', (req, res) => {
+  //get at the session object and store it ina local variable
+  let sess_obj = req.session;
+  //set the value from the text field equal to the session object key 'favband'
+  sess_obj.favalbum = req.body.favourite;
+  //send the session object key 'favalbum' to the band.ejs template
+  res.render('myalbums', { data: sess_obj.favalbum });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running at port 3000");
 });
-
